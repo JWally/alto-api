2d7cc6b834898d061e513e49ab4bacb43a96ef37
Justin W. Wolcott <justin@wolcott.io>
2021-10-09 22:27:25 -0500

    remove some of the logic from getting a user and put it in utilities

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,6 @@ var utilities = require("./utilities");
 // ********************************************************************
 exports.get_info = function (req, res) {
 	//
-	// First make sure that the user themselves or someone with an "is_admin" cookie
-	// is requesting the data - otherwise fail them
-	//
-	if(req.params.user_id !== req.signedCookies.user_id || req.params.is_admin == true){
-		return utilities.fail(req, res, 403);
-	}
-	
-	//
 	// For this exercise we're storing password salt / hash directly on the user.
 	// There are better ways, but still it feels wrong sending any password info
 	// back to the user; so we're deleting it here...
