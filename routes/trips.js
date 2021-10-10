4cc391e3cc4a77e94acfc903383f43dba3aeaab9
Justin W. Wolcott <justin@wolcott.io>
2021-10-09 22:26:52 -0500

    build out of trips module

---
+++ routes/trips.js
@@ -0,0 +1,19 @@
+
+
+exports.create = function(req, res){
+	
+}
+
+
+exports.update = function(req, res){
+	
+}
+
+
+exports.get = function(req, res){
+	
+}
+
+exports.list = function(req, res){
+	
+}
