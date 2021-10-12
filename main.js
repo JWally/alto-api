0f6150868c6c7229389ba7009f7cc44286cf87e0
Justin W. Wolcott <justin@wolcott.io>
2021-10-12 05:33:52 -0500

    clean up, and add better fall through route to catch all methods (not just GET)

--- main.js
+++ main.js
@@ -1,49 +1,6 @@
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // *************************************************************************************************
-// ****************************************   DESCRIPTION    ***************************************
-// *************************************************************************************************
-// /////////////////////////////////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-	The process flow would look like this:
-	
-	1.) The client logs in via the API
-	
-	2.) The API returns user information to the client
-		so that they can create a trip
-		
-	3.) The user tells the API to create a trip
-
-	4.) The API hits services to:
-		- Find a driver
-		- Find a vehicle
-		- Calculate a time-estimate
-		- Calculate a cost-estimate
-		
-	5.) The user can get driver information 
-	
-	6.) The user then MUST update the trip to approve it
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-// /////////////////////////////////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////////////////////////////////
-// *************************************************************************************************
 // ****************************************      SET UP      ***************************************
 // *************************************************************************************************
 // /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -109,28 +66,6 @@ app.use(bodyParser.urlencoded({"extended": true}));
 // *************************************************************************************************
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-//
-// For brevity / getting work done, let's assume the following:
-//
-// 1.) There already exist ways to CRUD
-//     - users
-//     - drivers
-//     - vehicles
-//
-//
-// 2.) *MY OBJECTIVE* then is to build out routing for trips (calling it "routes" would be confusing)
-//     - Create a trip
-//     - Update a trip
-//     - Get a trip
-//     - Cancel a trip
-//
-//
-
-
-
 
 
 // -- --------------------------
@@ -295,9 +230,15 @@ app.delete("/trip/:user_id/:trip_request_time", function(req, res){
 // Fall Through Route - Throw a custom 404 at the user if their method / path don't
 // match anything we currently offer...
 //
+/*
 app.get("*", function (req, res) {
     routes.utilities.fail(req, res, 404);
 });
+*/
+
+app.use((req, res) => {
+	routes.utilities.fail(req, res, 404);
+})
 
 http.createServer(app).listen(app.get("port"), function () {
     console.log("Environment: ", app.get("env"));
